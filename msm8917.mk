b6cb11a60442f8aa001cac0e5fce47f4b5442091
andrew2193 <birthday@hints.or.kr>
2023-03-24 16:04:57 +0900

    msm8917-common: use lineage charger

--- msm8917.mk
+++ msm8917.mk
@@ -121,6 +121,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libsuspend
 
+# Charger animation
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/charger/animation.txt:$(TARGET_COPY_OUT_PRODUCT)/etc/res/values/charger/animation.txt \
+    $(LOCAL_PATH)/configs/charger/battery_scale.png:$(TARGET_COPY_OUT_PRODUCT)/etc/res/images/charger/battery_scale.png \
+    $(LOCAL_PATH)/configs/charger/battery_fail.png:$(TARGET_COPY_OUT_PRODUCT)/etc/res/images/charger/battery_fail.png \
+    $(LOCAL_PATH)/configs/charger/main_font.png:$(TARGET_COPY_OUT_PRODUCT)/etc/res/images/charger/main_font.png
+
 PRODUCT_PACKAGES += \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
