01427b627f12dd37dcb283fd1e573b835d40f40f
TheUbuntuUser <birthday@hints.or.kr>
2023-04-18 23:24:05 +0900

    remove fm radio

--- msm8917.mk
+++ msm8917.mk
@@ -245,16 +245,6 @@ PRODUCT_COPY_FILES += \
 #    android.hardware.keymaster@3.0-impl \
 #    android.hardware.keymaster@3.0-service
 
-# FMRadio deps
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/FMRadio/privapp_whitelist_com.caf.fmradio.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp_whitelist_com.caf.fmradio.xml \
-    $(LOCAL_PATH)/FMRadio/qcom.fmradio.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/qcom.fmradio.xml \
-    $(LOCAL_PATH)/FMRadio/libqcomfm_jni.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/lib/libqcomfm_jni.so \
-    $(LOCAL_PATH)/FMRadio/qcom.fmradio.jar:$(TARGET_COPY_OUT_SYSTEM)/framework/qcom.fmradio.jar
-
-# FMRadio app
-PRODUCT_PACKAGES += FM2Prebuilt
-
 # Low power Whitelist
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
