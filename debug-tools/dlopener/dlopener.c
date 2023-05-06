dccac6e5f862b635af883657011897ec251de70e
andrew2193 <birthday@hints.or.kr>
2023-05-06 13:51:42 +0900

    Revert "msm8917-common: dlopener: Use more common exit code"
    This reverts commit 1bebb435ddb0f390f062c4b9f64b390bec62c475.

--- debug-tools/dlopener/dlopener.c
+++ debug-tools/dlopener/dlopener.c
@@ -1,26 +1,27 @@
 #include <dlfcn.h>
+#include <errno.h>
 #include <stdio.h>
-#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
   const char *path;
+  int status = -EINVAL;
   void *handle = NULL;
-  int ret = EXIT_FAILURE;
 
   if (argc <= 1) {
     printf("Please specify a module to load!\n");
-    return ret;
+    return status;
   }
 
   path = argv[1];
 
   handle = dlopen(path, RTLD_NOW);
   if (handle == NULL) {
-    const char *err_str = dlerror();
+    char const *err_str = dlerror();
     printf("load: module=%s\n%s\n", path, err_str ? err_str : "unknown");
+    status = -EINVAL;
   } else {
     printf("load: module=%s %s\n", path, "Success!");
-    ret = EXIT_SUCCESS;
   }
-  return ret;
+
+  return status;
 }
