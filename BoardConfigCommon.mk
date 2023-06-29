9a3e8e3180cf465d6e190a703c9cc2898c004210
andrew2193 <birthday@hints.or.kr>
2023-06-29 16:09:40 +0900

    BoardConfigCommon.mk: change kernel Image Image.gz

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -47,7 +47,7 @@ BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom msm_rtb.filter=0x
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x01e00000
-BOARD_KERNEL_IMAGE_NAME := zImage
+BOARD_KERNEL_IMAGE_NAME := Image.gz
 BOARD_KERNEL_SEPARATED_DT := true
 TARGET_KERNEL_ARCH := arm
 TARGET_KERNEL_SOURCE := kernel/samsung/msm8917
